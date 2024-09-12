c614abc05aa1f6e2a23f9ef879a97dbd34426ff1
DvLucas <p.lucasbenitez@gmail.com>
2024-09-11 22:35:18 -0300

    add: images agents

---
+++ src/consts/agents.ts
@@ -0,0 +1,135 @@
+interface Agent {
+    name: string,
+    role: string,
+    selected: boolean,
+    key: string
+}
+
+export const AGENTS: Agent[] = 
+[
+    {
+    name: "Breach",
+    role: "INITIATOR",
+    selected: false,
+    key: "breach",
+  },{
+    name: "Brimstone",
+    role: "CONTROLLER",
+    selected: false,
+    key: "brimstone",
+  },{
+    name: "Cypher",
+    role: "SENTINEL",
+    selected: false,
+    key: "cypher",
+  },{
+    name: "Jett",
+    role: "DUELIST",
+    selected: false,
+    key: "jett",
+  },{
+    name: "Omen",
+    role: "CONTROLLER",
+    selected: false,
+    key: "omen",
+  },{
+    name: "Phoenix",
+    role: "DUELIST",
+    selected: false,
+    key: "phoenix",
+  },{
+    name: "Sage",
+    role: "SENTINEL",
+    selected: false,
+    key: "sage",
+  },{
+    name: "Sova",
+    role: "INITIATOR",
+    selected: false,
+    key: "sova",
+  },{
+    name: "Viper",
+    role: "CONTROLLER",
+    selected: false,
+    key: "viper",
+  },{
+    name: "Raze",
+    role: "DUELIST",
+    selected: false,
+    key: "raze",
+  },{
+    name: "Reyna",
+    role: "DUELIST",
+    selected: false,
+    key: "reyna",
+  },{
+    name: "Killjoy",
+    role: "SENTINEL",
+    selected: false,
+    key: "killjoy",
+  },{
+    name: "Skye",
+    role: "INITIATOR",
+    selected: false,
+    key: "skye",
+  },{
+    name: "Yoru",
+    role: "DUELIST",
+    selected: false,
+    key: "yoru",
+  },{
+    name: "Astra",
+    role: "CONTROLLER",
+    selected: false,
+    key: "astra",
+  },{
+    name: "KAY/O",
+    role: "INITIATOR",
+    selected: false,
+    key: "kayo",
+  },{
+    name: "Chamber",
+    role: "SENTINEL",
+    selected: false,
+    key: "chamber",
+  },{
+    name: "Neon",
+    role: "DUELIST",
+    selected: false,
+    key: "neon",
+  },{
+    name: "Fade",
+    role: "INITIATOR",
+    selected: false,
+    key: "fade",
+  },{
+    name: "Harbor",
+    role: "CONTROLLER",
+    selected: false,
+    key: "harbor",
+  },{
+    name: "Gekko",
+    role: "INITIATOR",
+    selected: false,
+    key: "gekko",
+  },{
+    name: "Deadlock ",
+    role: "SENTINEL",
+    selected: false,
+    key: "deadlock",
+  },{
+    name: "Iso",
+    role: "DUELIST",
+    selected: false,
+    key: "iso",
+  },{
+    name: "Clove",
+    role: "CONTROLLER",
+    selected: false,
+    key: "clove",
+  },{
+    name: "Vyze",
+    role: "SENTINEL",
+    selected: false,
+    key: "vyze",
+  },];
